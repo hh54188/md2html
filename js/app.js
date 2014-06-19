723aeb1d0e39c84220a3eda829ab50f5f77786d3
hh54188 <252438823@qq.com>
2014-06-19 18:07:55 +0800

    update

--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@ define(["./src/markdown"], function (markdown) {
 	console.log(markdown.prototype);
 	function Editor(input, preview) {
 		this.update = function () {
+			console.log(input.value.toString());
 	  		preview.innerHTML = markdown.toHTML(input.value);
-	  		console.log(markdown.parse(input.value));
 		};
 		input.editor = this;
 		this.update();
 	}
 	var $ = function (id) { return document.getElementById(id); };
-	new Editor($("text-input"), $("preview"));	
+	new Editor($("text-input"), $("preview"));
 });
